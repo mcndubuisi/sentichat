08b90d320cd881e1843d5d65698ff66a6b8f8a72
Emmanuel Ndubuisi <psynch45@gmail.com>
2018-05-14 18:50:31 +0100

    Add files via upload

---
+++ public/chat.js
@@ -0,0 +1,78 @@
+// Intiliaze Sentimood Library
+var sentimood = new Sentimood();
+// Empty array for finding average mood score
+var arr = [];
+
+// Generates random number for username extension
+function getRandomArbitrary(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+// Random username for users
+var username = 'User' + getRandomArbitrary(100, 1000);
+
+// Start jQuery
+$(function(){
+    // Loads socket.io-client
+    var socket = io();
+
+    // Sends input value to the server
+    $('button').click(function(){
+        socket.emit('chat message', $('#m').val());
+        $('#m').val('');
+        return false;
+    });
+
+    // Sends input value to the client
+    // Check message for sentiment
+    socket.on('chat message', function(msg){
+        var analysis = sentimood.analyze(msg);
+
+        // Ensures user does not send empty message 
+        // Adds animation to the send button
+        if(msg.length > 0){
+            $('.send-button').css("background-color", "#535353");
+            $('ul').append('<i class="material-icons arrow">arrow_drop_up</i>'+
+            '<li class="text-bubble"><p class="username"><strong>' + username +'</strong></p>'+ msg +'</li>');
+            setTimeout(function(){$('.send-button').css("background-color", "#404040")}, 200);
+        }else{
+            $('.send-button').css("background-color", "#ff3232")
+            setTimeout(function(){$('.send-button').css("background-color", "#404040"); alert('Message empty!');}, 200);
+        }
+
+        // Finds average sentiment score and changes background accordingly
+        function bgColor(){
+            var total = 0;
+            arr.push(analysis.score);
+            
+            for(i=0; i<arr.length; i++){
+                total += arr[i];
+                average = total/arr.length;
+            }
+            switch(average) {
+                case -5:
+                case -4:
+                case -3:
+                case -2:
+                    $("body").css("background-image", "url('/img/negative.png')");
+                    break;
+                case -1:
+                case 0:
+                case 1:
+                case 2:
+                    $("body").css("background", "url('/img/neutral.png')");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    $("body").css("background", "url('/img/positive.png')");
+                    break;
+            }
+        }
+
+        bgColor();
+    });
+
+});
+
+// End jQuery
---
+++ sentichat.js
@@ -0,0 +1,29 @@
+// Dependencies required for app
+var express = require('express');
+var app = express();
+
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+// Use port 3000 unless there exists a preconfigured port
+app.set('port', process.env.PORT || 3000);
+
+// Static resources middleware
+app.use(express.static(__dirname + '/public'));
+
+// Route
+app.get('/', function(req, res){
+    res.render('index');
+});
+
+// Emits even from the server
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+      io.emit('chat message', msg)
+  });
+});
+
+// Listen on localhost:3000
+http.listen(app.get('port'), function(){
+    console.log('Express is listening at http://localhost:' + app.get('port'));
+});
