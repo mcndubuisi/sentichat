25ae794c0a1775a1fcc34342fcc15b34363b0624
Ndubuisi <mcndubuisi@icloud.com>
2018-06-27 18:07:35 +0100

    Set view engine to handelbars

--- public/chat.js
+++ public/chat.js
@@ -5,19 +5,21 @@ var arr = [];
 
 // Generates random number for username extension
 function getRandomArbitrary(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
+    return Math.floor(Math.random() * (max - min) + min);
 }
 
 // Random username for users
 var username = 'User' + getRandomArbitrary(100, 1000);
 
 // Start jQuery
-$(function(){
+$(function() {
     // Loads socket.io-client
     var socket = io();
 
+    socket.emit('send-username', username);
+
     // Sends input value to the server
-    $('button').click(function(){
+    $('button').click(function() {
         socket.emit('chat message', $('#m').val());
         $('#m').val('');
         return false;
@@ -25,46 +27,52 @@ $(function(){
 
     // Sends input value to the client
     // Check message for sentiment
-    socket.on('chat message', function(msg){
+    socket.on('chat message', function(msg) {
         var analysis = sentimood.analyze(msg);
 
         // Ensures user does not send empty message 
         // Adds animation to the send button
-        if(msg.length > 0){
+        if (msg.length > 0) {
             $('.send-button').css("background-color", "#535353");
-            $('ul').append('<i class="material-icons arrow">arrow_drop_up</i>'+
-            '<li class="text-bubble"><p class="username"><strong>' + username +'</strong></p>'+ msg +'</li>');
-            setTimeout(function(){$('.send-button').css("background-color", "#404040")}, 200);
-        }else{
-            $('.send-button').css("background-color", "#ff3232")
-            setTimeout(function(){$('.send-button').css("background-color", "#404040"); alert('Message empty!');}, 200);
+            $('ul').append('<i class="material-icons arrow">arrow_drop_up</i>' +
+                '<li class="text-bubble"><p class="username"><strong>' + username + '</strong></p>' + msg + '</li>');
+            setTimeout(function() { $('.send-button').css("background-color", "#404040"); }, 200);
+        } else {
+            $('.send-button').css("background-color", "#ff3232");
+            setTimeout(function() {
+                $('.send-button').css("background-color", "#404040");
+                alert('Message empty!');
+            }, 200);
         }
 
         // Finds average sentiment score and changes background accordingly
-        function bgColor(){
+        function bgColor() {
             var total = 0;
             arr.push(analysis.score);
-            
-            for(i=0; i<arr.length; i++){
+
+            for (i = 0; i < arr.length; i++) {
                 total += arr[i];
-                average = Math.floor(total/arr.length);
+                average = Math.floor(total / arr.length);
             }
-            switch(average) {
+            switch (average) {
                 case -5:
                 case -4:
                 case -3:
                 case -2:
                 case -1:
+                    // socket.emit('background image', "url('/img/negative.png')");
                     $("body").css("background-image", "url('/img/negative.png')");
                     break;
                 case 0:
                 case 1:
+                    // socket.emit('background image', "url('/img/neutral.png')");
                     $("body").css("background", "url('/img/neutral.png')");
                     break;
                 case 2:
                 case 3:
                 case 4:
                 case 5:
+                    // socket.emit('background image', "url('/img/positive.png')");
                     $("body").css("background", "url('/img/positive.png')");
                     break;
             }
--- sentichat.js
+++ sentichat.js
@@ -1,29 +1,44 @@
+/*jshint esversion: 6 */
+
 // Dependencies required for app
 const express = require('express');
 const app = express();
 
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+var users = [];
 
 // Use port 3000 unless there exists a preconfigured port
 app.set('port', process.env.PORT || 3000);
+
+// Set up handlebars view engine
+
+var handlebars = require('express3-handlebars')
+    .create({ defaultLayout: 'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 
 // Static resources middleware
 app.use(express.static(__dirname + '/public'));
 
 // Route
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
     res.render('index');
 });
 
-// Emits even from the server
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-      io.emit('chat message', msg)
-  });
+// Emits event from the server
+io.on('connection', function(socket) {
+    socket.on('chat message', function(msg) {
+        io.emit('chat message', msg);
+    });
+    socket.on('send-username', function(username) {
+        socket.username = username;
+        users.push(username);
+        console.log(users);
+    });
 });
 
 // Listen on localhost:3000
-http.listen(app.get('port'), function(){
+http.listen(app.get('port'), function() {
     console.log('Express is listening at http://localhost:' + app.get('port'));
 });
